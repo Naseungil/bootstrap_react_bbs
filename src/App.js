b650425fe5effbabea7f5e43b58a7454e84945f1
Naseungil <sktmddlf505@naver.com>
2024-11-04 10:36:20 +0900

    Write.js import

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import BoardList from "./BoardList";
+import Write from "./Write";
 
 function App() {
   return (
     <div className="container">
       <h1>React Board</h1>
       <BoardList />
+      <Write />
     </div>
   );
 }
